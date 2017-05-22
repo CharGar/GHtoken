2999fc7cc4b760881af08f9513aa204fe9e07d2f
Charlie Garnaas <cgarnaas555@gmail.com>
2017-05-22 09:30:19 -0500

    port fix

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var port = process.env.PORT||3004;
+
 // requires express
 var express = require('express');
 //requires path
@@ -6,12 +6,14 @@ var path = require('path');
 //shorthand express varibale
 var app = express();
 // uses static files
+var port = process.env.PORT||3004;
+
 app.use(express.static('public'));
 // /* gets all follows the path to index.html
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/views/index.html'));
 });
 // Tells the sever to listen for action on port 3004
-app.listen(PORT, function() {
+app.listen(port, function() {
   console.log('up port');
 });
