d8e28157d48832a523a7a36cfd5e3184a49478ca
Charlie Garnaas <cgarnaas555@gmail.com>
2018-02-27 12:53:35 -0600

    new push

--- public/scripts/github.service.js
+++ public/scripts/github.service.js
@@ -2,6 +2,7 @@ myApp.service('GithubAPI', function($http) {
   //Enter your token and username here:
   var oauthToken = '580a25cb6dc3e93a787bf12abd5eb94c4e2c3c0a';
   var username = 'chargar';
+  
 
   //Call to Github API to fetch user's profile info
   this.githubProfile = function(){
@@ -25,7 +26,7 @@ myApp.service('GithubAPI', function($http) {
       method: 'GET',
       url: 'https://api.github.com/users/' + username + '/repos',
       headers: {
-        'Authorization': 'token '+ oauthToken}
+        'Authorization': 'token ' + oauthToken}
       }
     ).then(function(response) {
       console.log(response.data);
