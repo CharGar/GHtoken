ee2dd53ffed2597b872647b912eb51032aeb98e7
Charlie Garnaas <cgarnaas555@gmail.com>
2018-04-07 12:12:57 -0500

    new push

---
+++ public/scripts/controllers/home.js
@@ -0,0 +1,14 @@
+
+// myApp.controller('homeController', function ($http){
+//     console.log('ProfileController up and running!');
+//     vm = this;
+//     vm.info =[];
+  
+//     vm.count = function(){
+//     //   GithubAPI.githubProfile().then(function(data){
+//         vm.info = data;
+//       });
+//     };
+//     vm.getInfo();
+//   });
+  
